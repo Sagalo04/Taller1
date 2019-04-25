617fb621e519f1d6adc91560a2b97483aada1e83
Skele1028 <30535008+Skele1028@users.noreply.github.com>
2019-04-25 10:04:01 -0500

    Consultas
    FXML y algo del controller

--- src/Control/ConsultaController.java
+++ src/Control/ConsultaController.java
@@ -7,7 +7,17 @@ package Control;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -16,6 +26,11 @@ import javafx.fxml.Initializable;
  */
 public class ConsultaController implements Initializable {
 
+    @FXML
+    TextField txClienteC;
+    TextField txActorC;
+    TextField txFechaInicial;
+    TextField txFechaFinal;
     /**
      * Initializes the controller class.
      */
@@ -23,5 +38,34 @@ public class ConsultaController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    @FXML
+    private void btCancelar(ActionEvent event) {
+        try {
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+            Parent parent = FXMLLoader.load(getClass().getResource("/Vista/FXMLDocument.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(parent);
+            //stage.setTitle("Login");
+
+            stage.setScene(scene);
+
+            stage.show();
+
+        } catch (Exception e) {
+            
+        }
+
+    }
+    
+    @FXML
+    private void onBuscar(ActionEvent event) {
+        try {
+               //Para buscar
+              JOptionPane.showMessageDialog(null, "Busco");
+        } catch (Exception e) {
+            
+        }
+    }
     
 }
