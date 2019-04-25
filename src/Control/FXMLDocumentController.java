8eebaf5fd74e8a6d587c24481c2efbdd1586d37b
Sagalo04 <49987163+Sagalo04@users.noreply.github.com>
2019-04-25 09:21:27 -0500

    Botones FXM

--- src/Control/FXMLDocumentController.java
+++ src/Control/FXMLDocumentController.java
@@ -9,27 +9,78 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
  * @author admin-sala3
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleButtonAction1(ActionEvent event) {
+
+        try {
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+            Parent parent = FXMLLoader.load(getClass().getResource("/Vista/Insertar.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(parent);
+            //stage.setTitle("Login");
+
+            stage.setScene(scene);
+
+            stage.show();
+
+        } catch (Exception e) {
+        }
+
     }
-    
+
+    @FXML
+    private void handleButtonAction2(ActionEvent event) {
+        try {
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+            Parent parent = FXMLLoader.load(getClass().getResource("/Vista/Modificar.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(parent);
+            //stage.setTitle("Login");
+
+            stage.setScene(scene);
+
+            stage.show();
+
+        } catch (Exception e) {
+        }
+
+    }
+
+    @FXML
+    private void handleButtonAction3(ActionEvent event) {
+        try {
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+            Parent parent = FXMLLoader.load(getClass().getResource("/Vista/Consulta.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(parent);
+            //stage.setTitle("Login");
+
+            stage.setScene(scene);
+
+            stage.show();
+
+        } catch (Exception e) {
+        }
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
