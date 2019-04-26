838022f9ae3e15ff491ef9391d939d8e08dda52f
angela77777 <49988800+angela77777@users.noreply.github.com>
2019-04-26 15:09:58 -0500

    cancel Mod

--- src/Control/ModificarController.java
+++ src/Control/ModificarController.java
@@ -7,7 +7,14 @@ package Control;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -23,6 +30,24 @@ public class ModificarController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     
-    }    
+    } 
+    
+        @FXML
+    private void onCancel(ActionEvent event) {
+        try {
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+            Parent parent = FXMLLoader.load(getClass().getResource("/Vista/FXMLDocument.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(parent);
+            //stage.setTitle("Login");
+
+            stage.setScene(scene);
+
+            stage.show();
+
+        } catch (Exception e) {
+        }
+
+    }
     
 }
