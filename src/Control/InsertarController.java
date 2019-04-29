1a26d689440d7f29b44c9ff1157a95a8184c8aba
Sagalo04 <49987163+Sagalo04@users.noreply.github.com>
2019-04-28 23:40:24 -0500

    Update InsertarController.java

--- src/Control/InsertarController.java
+++ src/Control/InsertarController.java
@@ -85,50 +85,71 @@ public class InsertarController implements Initializable {
 
     @FXML
     private void onContinuar(ActionEvent event) {
-
-        String fech = Txfecha.getValue().toString();
-        String hour = Txhora.getText();
-
-        String sp1[] = fech.split("-");
-
-        for (int i = 0; i < sp1.length; i++) {
-
-            System.out.println(sp1[i]);
-
-        }
-
-        String sp2[] = hour.split(":");
-
-        for (int i = 0; i < sp2.length; i++) {
-
-            System.out.println(sp2[i]);
-
-        }
-
-        Timestamp time = new Timestamp(Integer.parseInt(sp1[0]) - 1900, Integer.parseInt(sp1[1]) - 1,
-                Integer.parseInt(sp1[2]), Integer.parseInt(sp2[0]),
-                Integer.parseInt(sp2[1]), Integer.parseInt(sp2[2]), 0);
-
-        //se crea un objeto de controlPelicula con constructor vacio
-        ControlPelicula objCP = new ControlPelicula();
-        //Se crea una Pelicula nula
-        Pelicula objP = null;
-
-        boolean ins = false;
-
-        //Se llena el objeto de cuetna con todos sus atributos
         try {
-            objP = new Pelicula(Txtitulo.getText(), Txdescripcion.getText(), Integer.parseInt(Txanio.getText()), Integer.parseInt(Txlenguaje.getText()),
-                    Integer.parseInt(Txlenguajeorg.getText()), Integer.parseInt(Txrentalduration.getText()), Double.parseDouble(Txrentalrate.getText()),
-                    Integer.parseInt(Txduration.getText()), Double.parseDouble(Txcosto.getText()), Txrestriccion.getText(), Txcontent.getText(), time);
+            String description = null;
+            int release_year = 0;
+            int original_language_id = 1;
+            int length = 0;
+            String rating = null;
+            String special_features = null;
+
+            if (!"".equals(Txdescripcion.getText())) {
+                description = Txdescripcion.getText();
+            }
+            if (!"".equals(Txrestriccion.getText())) {
+                rating = Txrestriccion.getText();
+            }
+            if (!"".equals(Txcontent.getText())) {
+                special_features = Txcontent.getText();
+            }
+            if (!"".equals(Txanio.getText())) {
+                release_year = Integer.parseInt(Txanio.getText());
+            }
+            if (!"".equals(Txlenguajeorg.getText())) {
+                original_language_id = Integer.parseInt(Txlenguajeorg.getText());
+            }
+            if (!"".equals(Txduration.getText())) {
+                length = Integer.parseInt(Txduration.getText());
+            }
+
+            String fech = Txfecha.getValue().toString();
+            String hour = Txhora.getText();
+
+            String sp1[] = fech.split("-");
+
+            for (int i = 0; i < sp1.length; i++) {
+
+                System.out.println(sp1[i]);
+
+            }
+
+            String sp2[] = hour.split(":");
+
+            for (int i = 0; i < sp2.length; i++) {
+
+                System.out.println(sp2[i]);
+
+            }
+
+            Timestamp time = new Timestamp(Integer.parseInt(sp1[0]) - 1900, Integer.parseInt(sp1[1]) - 1,
+                    Integer.parseInt(sp1[2]), Integer.parseInt(sp2[0]),
+                    Integer.parseInt(sp2[1]), Integer.parseInt(sp2[2]), 0);
+
+            //se crea un objeto de controlPelicula con constructor vacio
+            ControlPelicula objCP = new ControlPelicula();
+            //Se crea una Pelicula nula
+            Pelicula objP = null;
+
+            boolean ins = false;
+
+            //Se llena el objeto de cuetna con todos sus atributos
+            objP = new Pelicula(Txtitulo.getText(), description, release_year, Integer.parseInt(Txlenguaje.getText()),
+                    original_language_id, Integer.parseInt(Txrentalduration.getText()), Double.parseDouble(Txrentalrate.getText()),
+                    length, Double.parseDouble(Txcosto.getText()), rating, special_features, time);
 
             //Se llama al metodo de controlcuenta para insertar
             ins = objCP.insertarPelicula(objP);
-        } catch (Exception e) {
-            System.out.println("ERROR " + e.toString());
-        }
 
-        try {
             ((Node) (event.getSource())).getScene().getWindow().hide();
             Parent parent = FXMLLoader.load(getClass().getResource("/Vista/FXMLDocument.fxml"));
             Stage stage = new Stage();
@@ -138,8 +159,8 @@ public class InsertarController implements Initializable {
             stage.setScene(scene);
 
             stage.show();
-
         } catch (Exception e) {
+            System.out.println("ERROR " + e.toString());
         }
 
     }
