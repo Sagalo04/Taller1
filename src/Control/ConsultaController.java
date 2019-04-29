460bc26d183ab671e0c5d57f4d35a2a5bdde4e55
angela77777 <49988800+angela77777@users.noreply.github.com>
2019-04-29 15:56:05 -0500

    genero

--- src/Control/ConsultaController.java
+++ src/Control/ConsultaController.java
@@ -82,13 +82,8 @@ public class ConsultaController implements Initializable {
         String idCliente = txClienteC.getText();
         String nomCliente = txClienteC.getText();
         String TituloPeli = "";
-        String nomActor = txActorC.getText();
-        String genero = mnGenero.getText();
-        String fechaIni = txFechaInicial.getText();
-        String fechaFinal = txFechaFinal.getText();
         
         ConnectBD con = new ConnectBD();
-        
         String sql = null;
         /*String sql = "SELECT title\n" +
         "FROM sakila.film PI I NNER JOIN sakila.inventory I ON PI.film_id = I.film_id \n" +
@@ -107,34 +102,7 @@ public class ConsultaController implements Initializable {
                     "WHERE rental.customer_id=customer.customer_id AND customer.first_name =" + nomCliente;
             System.out.println(nomCliente);      
         }
-        else if(nomActor != null){
-            sql = "SELECT f.title\n" +
-                    "FROM  film f\n" +
-                    "JOIN film_actor fa \n" +
-                    "ON  f.film_id = fa.film_id\n" +
-                    "WHERE fa.actor_id IN ( \n" +
-                    "Select actor_id \n" +
-                    "FROM actor \n" +
-                    "WHERE concat(actor.first_name, \" \", actor.last_name) =" + nomActor;
-            System.out.println(nomActor);
-        }
-        else if(genero != null){
-                
-            sql = "SELECT f.title\n" +
-                    "FROM film f \n" +
-                    "INNER JOIN film_category fc ON f.film_id = fc.film_id\n" +
-                    "INNER JOIN category c ON fc.category_id = c.category_id\n" +
-                    //"WHERE c.name =" + genero ;
-                    "WHERE c.name = 'Horror'" ;            
-        }
-        else if(fechaIni != null && fechaFinal != null){
-            //SQL para  Todas las rentas realizadas en un periodo de tiempo.
-            /* OJO = No me sale el calendario ._. no puedo llamarlo */
-            sql =  "SELECT film.title as Pelicula,rental.rental_id as NumeroRenta, concat(customer.first_name,\" \",customer.last_name) as cliente ,rental.rental_date as FechaInicial,rental.return_date as FechaFinal \n"+
-                    "FROM rental \n"+
-                    "INNER JOIN customer INNER JOIN film \n"+
-                    "WHERE rental.customer_id=customer.customer_id AND rental.rental_date BETWEEN BETWEEN '2005-05-25 00:19:27 ' AND '2005-05-25 06:44:53'";            
-        }
+        
         //--------
         ResultSet rs = null;
         String r = "";
@@ -164,16 +132,69 @@ public class ConsultaController implements Initializable {
     @FXML
     private void onBuscar1(ActionEvent event) {
         
+        String nomActor = txActorC.getText();
+        
+        ConnectBD con = new ConnectBD();
+        String sql = null;
+        
+        if(nomActor != null){
+            sql = "SELECT f.title\n" +
+                    "FROM  film f\n" +
+                    "JOIN film_actor fa \n" +
+                    "ON  f.film_id = fa.film_id\n" +
+                    "WHERE fa.actor_id IN ( \n" +
+                    "Select actor_id \n" +
+                    "FROM actor \n" +
+                    "WHERE concat(actor.first_name, \" \", actor.last_name) =" + nomActor;
+            System.out.println(nomActor);
+            
+        ResultSet rs = null;
+        String r = "";
+        }
     }
     
     @FXML
     private void onBuscar2(ActionEvent event) {
         
+        String genero = mnGenero.getText();
+        
+        ConnectBD con = new ConnectBD();
+        String sql = null;
+        
+        if(genero != null){
+                
+            sql = "SELECT f.title\n" +
+                    "FROM film f \n" +
+                    "INNER JOIN film_category fc ON f.film_id = fc.film_id\n" +
+                    "INNER JOIN category c ON fc.category_id = c.category_id\n" +
+                    //"WHERE c.name =" + genero ;
+                    "WHERE c.name = 'Horror'" ;  
+            
+        ResultSet rs = null;
+        String r = "";
+        }
     }
     
     @FXML
     private void onBuscar3(ActionEvent event) {
         
+        String fechaIni = txFechaInicial.getText();
+        String fechaFinal = txFechaFinal.getText();
+        
+        ConnectBD con = new ConnectBD();
+        String sql = null;
+        
+        if(fechaIni != null && fechaFinal != null){
+            //SQL para  Todas las rentas realizadas en un periodo de tiempo.
+            /* OJO = No me sale el calendario ._. no puedo llamarlo */
+            sql =  "SELECT film.title as Pelicula,rental.rental_id as NumeroRenta, concat(customer.first_name,\" \",customer.last_name) as cliente ,rental.rental_date as FechaInicial,rental.return_date as FechaFinal \n"+
+                    "FROM rental \n"+
+                    "INNER JOIN customer INNER JOIN film \n"+
+                    "WHERE rental.customer_id=customer.customer_id AND rental.rental_date BETWEEN BETWEEN '2005-05-25 00:19:27 ' AND '2005-05-25 06:44:53'";     
+            
+        ResultSet rs = null;
+        String r = "";
+        }
     }
 
 }
